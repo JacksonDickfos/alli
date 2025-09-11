2447b317f10029b61b1be8f5ccc44443bfece1bf
Jackson <dickfosjackson@gmail.com>
2025-09-11 16:27:56 -0400

    Fix middle tab to use getLogoSource() instead of hardcoded logo.png

--- App.tsx
+++ App.tsx
@@ -272,7 +272,7 @@ function SignUpScreen({ navigation, onAuth }: any) {
   return (
     <View style={styles.authContainer}>
       <Image
-        source={require('./assets/logo.png')}
+        source={getLogoSource()}
         style={{ width: 180, height: 180, resizeMode: 'contain', marginBottom: 20 }}
       />
       {notice && <NoticeBanner message={notice.text} type={notice.type} />}
@@ -427,7 +427,7 @@ function LoginScreen({ navigation, onAuth }: any) {
   return (
     <View style={styles.authContainer}>
       <Image
-        source={require('./assets/logo.png')}
+        source={getLogoSource()}
         style={{ width: 180, height: 180, resizeMode: 'contain', marginBottom: 20 }}
       />
       {notice && <NoticeBanner message={notice.text} type={notice.type} />}
@@ -679,7 +679,7 @@ function AlliTabBarButton({ children, onPress }: AlliTabBarButtonProps) {
               }}
             >
               <Image
-                source={require('./assets/logo.png')}
+                source={getLogoSource()}
                 style={{ width: 36, height: 36, resizeMode: 'contain' }}
               />
             </View>
